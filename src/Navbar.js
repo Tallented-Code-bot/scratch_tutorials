fc4cde34e374ac5b15ac7d0e79f7b33260226eb5
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-06 07:05:10 -0700

    Create a basic navbar.

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar=()=>{
+	return (
+		<nav class="navbar">
+			<h1>The blog</h1>
+			<div class="links">
+				<a href="/">Home</a>
+				<a href="/create">New</a>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
