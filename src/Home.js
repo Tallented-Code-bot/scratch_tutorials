14186153a10b33c5d5fc0d8b4203053235a76f3d
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-06 11:29:29 -0700

    Add list of tutorials

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
+import {useState} from 'react';
+import TutorialList from './TutorialList';
+
+
 const Home=()=>{
+	const [tutorials,setTutorials]=useState([
+		{title:"Test number 1",body:"This is a test",author:"griffpatch",id:1},
+		{title:"Test number 2",body:"This is another test",author:"will_wam",id:2}
+	]);
 	return(
 		<div class="home">
-			<h2>Homepage</h2>		
+			<TutorialList tutorials={tutorials} title={"All tutorials"}/>
 		</div>
 	);
 }
---
+++ src/TutorialList.js
@@ -0,0 +1,19 @@
+const TutorialList = (props) =>{
+	const tutorials = props.tutorials;
+	const title=props.title;
+	return(
+		<div className="article_list">
+			<h1>{title}</h1>
+			{tutorials.map((tutorial)=>(
+				<div className="tutorial_preview" key={tutorial.id}>
+					<h2> {tutorial.title} </h2>
+					<p>By {tutorial.author}</p>
+				</div>
+			))}	
+
+		</div>
+
+	);
+}
+
+export default TutorialList;
