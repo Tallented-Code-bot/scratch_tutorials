927e0126b0e2abd613001228b0c602ab547be10f
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-06 09:20:55 -0700

    Update navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 const Navbar=()=>{
 	return (
 		<nav class="navbar">
-			<h1>The blog</h1>
-			<div class="links">
-				<a href="/">Home</a>
-				<a href="/create">New</a>
-			</div>
+			<h1>Scratch Tutorials</h1>
+			<a href="/">Home</a>
+			<a href="/create">Create</a>
+			<a href="/browse">Browse</a>
+
+			<a href="/login">Login</a>
 		</nav>
 	);
 }
