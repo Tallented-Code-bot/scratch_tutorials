d86dc0d9c4b007981a1883fee49040fb70844404
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-07 08:28:27 -0800

    Create a basic tutorial page.  No data yet.

---
+++ src/Tutorial.js
@@ -0,0 +1,14 @@
+import { useParams} from 'react-router-dom';
+
+
+const Tutorial=()=>{
+	const {id}=useParams();
+
+	return (
+		<div className="tutorial">
+			<h1>Tutorial - {id}</h1>
+		</div>
+	);
+};
+
+export default Tutorial;
