c77ce4f1dc7042a42cb64d39d087c0aa80f2e0a4
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-09 13:38:07 -0800

    Create basic get and post endpoints.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 //initialize variables
 const express = require("express");
 const mongoose = require("mongoose");
+const Tutorial = require("./models/tutorials.js");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
@@ -23,6 +24,31 @@ app.get("/api", (req, res) => {
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../build/", "index.html"));
+});
+
+app.post("/api/tutorials/", async (req, res) => {
+  console.log(req.query.author);
+  console.log(req.query.body);
+  const tutorial = new Tutorial({
+    author: req.query.author,
+    body: req.query.body,
+  });
+  try {
+    const newTutorial = await tutorial.save();
+    res.status(201).json(newTutorial);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+//Get all tutorials
+app.get("/api/tutorials/all/", async (req, res) => {
+  try {
+    const tutorials = await Tutorial.find();
+    res.json(tutorials);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 //listen on port 3000 by default
