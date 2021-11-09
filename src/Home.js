0db77c228be7883b47f99fb4c61c9decf43ae355
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-09 15:47:38 -0800

    Add http request to the server to get list of tutorials.

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,45 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TutorialList from "./TutorialList";
 import PropTypes from "prop-types";
 
 const Home = () => {
-  const [tutorials] = useState([
-    {
-      title: "Test number 1",
-      body: "This is a test",
-      author: "griffpatch",
-      id: 1,
-    },
-    {
-      title: "Test number 2",
-      body: "This is another test",
-      author: "will_wam",
-      id: 2,
-    },
-  ]);
+  const [tutorials, setTutorials] = useState(
+    /*[
+  {
+  title: "Test number 1",
+  body: "This is a test",
+  author: "griffpatch",
+  id: 1,
+  },
+  {
+  title: "Test number 2",
+  body: "This is another test",
+  author: "will_wam",
+  id: 2,
+  },
+  ]
+  */
+    null
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/tutorials/all/")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setTutorials(data);
+      });
+  }, []);
+
   return (
     <div className="home">
-      <TutorialList tutorials={tutorials} title={"All tutorials"} />
+      {console.log(tutorials)}
+      {tutorials && (
+        <TutorialList tutorials={tutorials} title={"All tutorials"} />
+      )}
     </div>
   );
 };
