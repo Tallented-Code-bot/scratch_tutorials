37cc5a87a705498661782a948eb81f3ced091a54
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-10 13:08:47 -0800

    Add markdown rendering.

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,12 +1,14 @@
 import { React, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import "./tutorial.css";
 
 const Tutorial = () => {
   const { id } = useParams();
   const [tutorial, setTutorial] = useState(null);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/tutorials/${id}`)
+    fetch(`http://192.168.212.52:8080/api/tutorials/${id}`)
       .then((res) => {
         return res.json();
       })
@@ -15,11 +17,14 @@ const Tutorial = () => {
         setTutorial(data);
       });
   }, []);
+
   return (
     <div className="tutorial">
       {tutorial && <h1>{tutorial[0].title}</h1>}
       {tutorial && <h3>By {tutorial[0].author}</h3>}
-      {tutorial && <p>{tutorial[0].body}</p>}
+      {tutorial && (
+        <ReactMarkdown>{tutorial[0].body.replace(/\\n/gi, "\n")}</ReactMarkdown>
+      )}
     </div>
   );
 };
