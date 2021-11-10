9ff9943124c2e813094d090525ed218c1671ca6a
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-09 16:13:12 -0800

    Make a basic tutorial page with data from the server.

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,12 +1,25 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const Tutorial = () => {
   const { id } = useParams();
+  const [tutorial, setTutorial] = useState(null);
 
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/tutorials/${id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setTutorial(data);
+      });
+  }, []);
   return (
     <div className="tutorial">
-      <h1>Tutorial - {id}</h1>
+      {tutorial && <h1>{tutorial[0].title}</h1>}
+      {tutorial && <h3>By {tutorial[0].author}</h3>}
+      {tutorial && <p>{tutorial[0].body}</p>}
     </div>
   );
 };
