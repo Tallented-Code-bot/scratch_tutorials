e92e1d64541f9aa9e39a54bd10d0e83abe5f43b5
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-10 06:11:55 -0800

    Fix a bug where key was not read correctly

--- src/TutorialList.js
+++ src/TutorialList.js
@@ -8,7 +8,7 @@ const TutorialList = (props) => {
     <div className="article_list">
       <h1>{title}</h1>
       {tutorials.map((tutorial) => (
-        <div className="tutorial_preview" key={tutorial.id}>
+        <div className="tutorial_preview" key={tutorial._id}>
           <Link to={`/tutorials/${tutorial._id}`}>
             <h2> {tutorial.title} </h2>
             <p>By {tutorial.author}</p>
