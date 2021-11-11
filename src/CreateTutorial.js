32f226a1412565792c3f1f5ccef82554d7f3e0f0
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-10 17:11:36 -0800

    Add creating tutorials.

---
+++ src/CreateTutorial.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+
+const CreateTutorial = () => {
+  const [markdown, setMarkdown] = useState("");
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const tutorial = { body: markdown, author: "Calvin", title: title };
+    console.log(tutorial);
+
+    fetch("http://192.168.212.52:8080/api/tutorials/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(tutorial),
+    }).then(() => {
+      alert("New tutorial added successfully");
+    });
+  }
+
+  return (
+    <div>
+      <h1>Create a tutorial</h1>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          type="text"
+          required=""
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+
+        <textarea
+          id="markdown"
+          name=""
+          cols="60"
+          rows="10"
+          required
+          placeholder="Type here"
+          value={markdown}
+          onChange={(e) => {
+            setMarkdown(e.target.value);
+          }}
+        ></textarea>
+        <input type="submit" value="Submit" />
+      </form>
+
+      <h3>Preview</h3>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
+    </div>
+  );
+};
+
+export default CreateTutorial;
