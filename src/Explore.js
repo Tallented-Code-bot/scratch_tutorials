5ab3de33b32e9f25c0e27b659a4c9b1bdb6f9896
Calvinn Tallent <calvintallentspam@gmail.com>
2021-11-14 19:38:01 -0800

    Add searching.

--- src/Explore.js
+++ src/Explore.js
@@ -1,21 +1,33 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import useQueryString from "./hooks/useQueryString";
 import Searchbar from "./Searchbar";
+import TutorialList from "./TutorialList";
 
 const Explore = () => {
   const [search, setSearch] = useQueryString("search");
+  const [tutorials, setTutorials] = useState(null);
+
+  const fetchData = async function () {
+    const data = await fetch(
+      `http://${window.location.hostname}:3390/api/tutorials/search?search=${search}`
+    );
+    const json = await data.json();
+    setTutorials(json);
+    console.log(tutorials);
+  };
+
+  useEffect(() => {
+    console.log("This is the useeffect");
+    console.log(search);
+    fetchData();
+  }, [search]);
 
   return (
     <div className="explore">
       <h1>Explore</h1>
       <Searchbar setSearch={setSearch} />
-      <input
-        type="text"
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
-      />
-      <p>{search}</p>
+      {tutorials && <TutorialList title={search} tutorials={tutorials} />}
     </div>
   );
 };
